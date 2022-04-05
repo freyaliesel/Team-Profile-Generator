fe90e0574e3002ad9e2e78826b3bc484b04b3ff1
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-05 11:53:09 -0500

    Intern test and class created, all tests pass

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -4,7 +4,6 @@ describe("Engineer", () => {
     describe("Initialization", () => {
         it("should create an object with a name, id, email, and GitHub username if provided valid arguments", () => {
             const engineer = new Engineer("Jane", 1, "jane@email.com", "janecodes");
-
             expect(engineer.name).toEqual("Jane");
             expect(engineer.id).toEqual(1);
             expect(engineer.email).toEqual("jane@email.com");
@@ -13,23 +12,19 @@ describe("Engineer", () => {
         it("should throw an error if not provided a github username", () => {
             const cb = () => new Engineer("Jane", 1, "jane@email.com");
             const err = new Error("Expected parameter 'github' to be a non-empty string")
-
             expect(cb).toThrowError(err);
         })
         it("should throw an error if 'github' is not a string", () => {
             const cb = () => new Engineer("Jane", 1, "jane@email.com", 4);
             const err = new Error("Expected parameter 'github' to be a non-empty string");
-
             expect(cb).toThrow(err);
         });
         it("should return 'Engineer' when getRole() is called", () => {
             const engineer = new Engineer("Jane", 1, "jane@email.com", "janecodes");
-
             expect(engineer.getRole()).toEqual("Engineer");
         });
         it("should return the engineer's github username when getRole() is called", () => {
             const engineer = new Engineer("Jane", 1, "jane@email.com", "janecodes");
-
             expect(engineer.getGithub()).toEqual("janecodes");
         });
     })
---
+++ tests/Intern.test.js
@@ -0,0 +1,31 @@
+const Intern = require("../lib/Intern.js");
+
+describe("Intern", () => {
+    describe("Initialization", () => {
+        it("should create an object with a name, id, email, and school if provided valid arguments", () => {
+            const intern = new Intern("Jane", 1, "jane@email.com", "Northwestern University");
+            expect(intern.name).toEqual("Jane");
+            expect(intern.id).toEqual(1);
+            expect(intern.email).toEqual("jane@email.com");
+            expect(intern.school).toEqual("Northwestern University");
+        });
+        it("should throw an error if not provided a school", () => {
+            const cb = () => new Intern("Jane", 1, "jane@email.com");
+            const err = new Error("Expected parameter 'school' to be a non-empty string")
+            expect(cb).toThrowError(err);
+        })
+        it("should throw an error if 'school' is not a string", () => {
+            const cb = () => new Intern("Jane", 1, "jane@email.com", 4);
+            const err = new Error("Expected parameter 'school' to be a non-empty string");
+            expect(cb).toThrow(err);
+        });
+        it("should return 'Intern' when getRole() is called", () => {
+            const intern = new Intern("Jane", 1, "jane@email.com", "Northwestern University");
+            expect(intern.getRole()).toEqual("Intern");
+        });
+        it("should return the intern's school username when getSchool() is called", () => {
+            const intern = new Intern("Jane", 1, "jane@email.com", "Northwestern University");
+            expect(intern.getSchool()).toEqual("Northwestern University");
+        });
+    })
+})
