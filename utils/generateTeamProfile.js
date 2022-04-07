7c87983d4e076486e91c0d15e4dfb9de5139da53
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-07 13:56:22 -0500

    function writing in progress

--- utils/generateTeamProfile.js
+++ utils/generateTeamProfile.js
@@ -1,13 +1,19 @@
+const fs = require("fs");
 const Engineer = require("../lib/Engineer.js");
 const Manager = require("../lib/Manager.js");
 const Intern = require("../lib/Intern.js");
 
-function generateTeamProfile(rawTeam) {
-    console.log(rawTeam);
+function generateTeamProfile(data) {
+    console.log(data);
+    makeEmployeeObjects(data);
+    return JSON.stringify(team);
+}
+
+function makeEmployeeObjects(rawTeam) {
     const team = [];
     rawTeam.forEach((member) => {
         let teamMember;
-        switch (member.role) {
+        switch (member.getRole()) {
             case "Manager":
                 teamMember = new Manager(
                     member.name,
@@ -39,8 +45,45 @@ function generateTeamProfile(rawTeam) {
                 break;
         }
     });
-    console.log(team);
-    return JSON.stringify(team);
+    return team;
 }
 
-module.exports = { generateTeamProfile };
+function createTeamCards(team) {
+    let html = [];
+    team.forEach((employee) => {
+        html.push(renderCard(employee));
+        // create card for each employee
+        // put in the document
+    });
+    return html.join("");
+}
+
+function renderCard(employee) {
+    // creates the html
+}
+
+function renderIcon() {}
+
+function renderInfo(employee) {
+    let info = "";
+    switch (employee.getRole()) {
+        case "Manager":
+            info = `Office Number: ${employee.officeNumber}`
+            break;
+        case "Engineer":
+            info = `GitHub: <a href="http://github.com/${employee.github}">${employee.github}</a>`
+            break;
+        case "Intern":
+            info = ``
+            break;
+    }
+    return info;
+}
+
+module.exports = {
+    generateTeamProfile,
+    makeEmployeeObjects,
+    createTeamCards,
+    renderCard,
+    renderIcon,
+};
