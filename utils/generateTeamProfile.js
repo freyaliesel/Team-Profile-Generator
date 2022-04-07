88c04f1bbe1830633b7077ffd59823b373f0be33
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-06 19:21:59 -0500

    creates array of employee objects based on user input

--- utils/generateTeamProfile.js
+++ utils/generateTeamProfile.js
@@ -1,4 +1,44 @@
-function generateTeamProfile(team) {
+const Engineer = require("../lib/Engineer.js");
+const Manager = require("../lib/Manager.js");
+const Intern = require("../lib/Intern.js");
+
+function generateTeamProfile(rawTeam) {
+    console.log(rawTeam);
+    const team = [];
+    rawTeam.forEach((member) => {
+        let teamMember;
+        switch (member.role) {
+            case "Manager":
+                teamMember = new Manager(
+                    member.name,
+                    member.id,
+                    member.email,
+                    member.officeNumber
+                );
+                team.push(teamMember);
+                break;
+            case "Engineer":
+                teamMember = new Engineer(
+                    member.name,
+                    member.id,
+                    member.email,
+                    member.github
+                );
+                team.push(teamMember);
+                break;
+            case "Intern":
+                teamMember = new Intern(
+                    member.name,
+                    member.id,
+                    member.email,
+                    member.school
+                );
+                team.push(teamMember);
+                break;
+            default:
+                break;
+        }
+    });
     console.log(team);
 }
 
